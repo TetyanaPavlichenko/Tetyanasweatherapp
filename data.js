1967d10967ffb5992a1ccb68898b3783a09c1a1c
TetyanaPavlichenko <109222727+TetyanaPavlichenko@users.noreply.github.com>
2022-07-26 20:23:58 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,19 +1,10 @@
-function getForecast(coordinates) {
-  console.log(coordinates);
-  let key = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}=&appid=${key}&units=metric`;
-  console.log(apiURL);
-  axios.get(apiURL).then(displayForecast);
-}
+
 function displayForecast(response) {
   console.log(response.data);
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
   forecast.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
@@ -34,8 +25,18 @@ function displayForecast(response) {
           <div class="weather-forecast-temperature-min"> ${forecastDay.temp.min}° </div>
         </div>
       </div>
-  `;
-
+  `
+  }
+}
+  
+  function getForecast(coordinates) {
+  console.log(coordinates);
+  let key = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}=&appid=${key}&units=metric`;
+  console.log(apiURL);
+  axios.get(apiURL).then(displayForecast);
+}
+  
 function displayWeather(response) {
   let weatherShow = document.querySelector("#val");
   celsiusTemperature = response.data.main.temp;
