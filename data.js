668c469c96c6ad821eadd0c7545c16696dacd3be
TetyanaPavlichenko <109222727+TetyanaPavlichenko@users.noreply.github.com>
2022-07-26 12:14:39 +0300

    Update data.js

--- data.js
+++ data.js
@@ -67,11 +67,10 @@ function displayForecast() {
         <img
           src="http://openweathermap.org/img/wn/50d@2x.png"
           alt=""
-          width="42"
+          width="50"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> 18° </span>
-          <span class="weather-forecast-temperature-min"> 12° </span>
+          <span class="weather-forecast-temperature-max"> 18° </span> <span class="weather-forecast-temperature-min"> 12° </span>
         </div>
       </div>
   `;
