1018c1e03f2f2b6dbd19c7027eaa7f3a60587613
TetyanaPavlichenko <109222727+TetyanaPavlichenko@users.noreply.github.com>
2022-07-26 12:16:02 +0300

    Update data.js

--- data.js
+++ data.js
@@ -62,7 +62,7 @@ function displayForecast() {
     forecastHTML =
       forecastHTML +
       `
-      <div class="col-4">
+      <div class="col-3">
         <div class="weather-forecast-date">${day}</div>
         <img
           src="http://openweathermap.org/img/wn/50d@2x.png"
