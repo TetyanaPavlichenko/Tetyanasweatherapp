ac592aea2cc5329daa4ed4307a215680302588bd
TetyanaPavlichenko <109222727+TetyanaPavlichenko@users.noreply.github.com>
2022-07-26 12:18:56 +0300

    work with css

--- data.js
+++ data.js
@@ -70,7 +70,8 @@ function displayForecast() {
           width="50"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> 18° </span> <span class="weather-forecast-temperature-min"> 12° </span>
+          <span class="weather-forecast-temperature-max"> 18° </span> 
+          <span class="weather-forecast-temperature-min"> 12° </span>
         </div>
       </div>
   `;
