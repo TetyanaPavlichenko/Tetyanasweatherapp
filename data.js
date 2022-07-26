1cbf7e35d8b5834a27649f8a5ffac182e531d2b3
TetyanaPavlichenko <109222727+TetyanaPavlichenko@users.noreply.github.com>
2022-07-26 12:29:47 +0300

    weather-forecast-temperature-span-to-div

--- data.js
+++ data.js
@@ -70,8 +70,8 @@ function displayForecast() {
           width="50"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> 18° </span> 
-          <span class="weather-forecast-temperature-min"> 12° </span>
+          <div class="weather-forecast-temperature-max"> 18° </div> 
+          <div class="weather-forecast-temperature-min"> 12° </div>
         </div>
       </div>
   `;
